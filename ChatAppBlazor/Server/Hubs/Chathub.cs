9995b22e4294da72a7433e4bcf626a242c9173c6
Samuel Stenerson <samueljstenerson@gmail.com>
2023-09-17 14:10:57 -0400

    Add project files.

---
+++ ChatAppBlazor/Server/Hubs/Chathub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ChatAppBlazor.Server.Hubs
+
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            Console.WriteLine($"Message from {user}: {message}");
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
